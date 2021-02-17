3c4a6aded018746bb597d9337711ffa877a39d77
Lynn Nguyen <55603454+meloly4@users.noreply.github.com>
2021-02-17 17:28:53 -0600

    Player animation, health & ammo HUD

---
+++ 292game/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PHealth : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          currentHealth = maxHealth;
+          healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //insert attack input here
+        if (Input.GetKeyDown(KeyCode.Space) && currentHealth > 0)
+        {
+          TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+      currentHealth -= damage;
+
+      healthBar.SetHealth(currentHealth);
+    }
+}
