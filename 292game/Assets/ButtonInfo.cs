d74079545fb811b3672cf727cc7a6060e17ed9cc
DK <Dikshyant99@gmail.com>
2021-02-20 15:52:30 -0600

    Made Shop UI

---
+++ 292game/Assets/ButtonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public Text Pricetxt;
+    public GameObject ShopManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pricetxt.text = "Cost: " + ShopManager.GetComponent<ShopManager>().shopItems[2,ItemID].ToString();
+        
+    }
+}
