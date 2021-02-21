32a79541ba90fba45968af7b96c247fbce7f5ec2
Lynn Nguyen <55603454+meloly4@users.noreply.github.com>
2021-02-21 12:06:46 -0600

    Attack interactions

---
+++ 292game/Assets/Script/SpreaderHpBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpreaderHpBar : MonoBehaviour
+{
+    public Slider slider;
+
+    void Update()
+    {
+        if (slider.value == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void SetMaxHp(int hp)
+    {
+        slider.maxValue = hp;
+        slider.value = hp;
+    }
+
+    public void SetHp(int hp)
+    {
+        slider.value = hp;
+    }
+}
