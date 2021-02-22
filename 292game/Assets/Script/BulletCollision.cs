5868197b32d24a787c8f3544b90dbb899cb74c90
Lynn Nguyen <55603454+meloly4@users.noreply.github.com>
2021-02-22 13:41:58 -0600

    Add cart UI

--- 292game/Assets/Script/BulletCollision.cs
+++ 292game/Assets/Script/BulletCollision.cs
@@ -8,18 +8,6 @@ public class BulletCollision : MonoBehaviour
 
     public int damage = 25;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D col) {
         if (col.gameObject.name=="Enemy" ){
             Destroy(this.gameObject);
---
+++ 292game/Assets/Script/Shelves Script/ShelveManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class ShelveManager : MonoBehaviour
+{
+   public int[,] shelveItems = new int[5,5];
+   public float score;
+   public Text scoretxt;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretxt.text= "Score:" + score;
+        
+        //IDs
+        shelveItems[1,1] = 1;
+        shelveItems[1,2] = 2;
+        shelveItems[1,3] = 3;
+        shelveItems[1,4] = 4;
+    }
+
+
+    public void BuyOnSelf(){
+
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Shelf Item").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        if (score >= shelveItems[2,ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        {
+            score -= shelveItems[2,ButtonRef.GetComponent<ButtonInfo>().ItemID];
+            scoretxt.text= "Score:" + score;
+        }
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
