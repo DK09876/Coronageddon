c9f8fea68b449f1928edca217f4defa4e18bd53f
Lynn Nguyen <55603454+meloly4@users.noreply.github.com>
2021-03-01 13:17:09 -0600

    Move HUD out of Player prefab

---
+++ 292game/Assets/Script/Shelves Script/PanelOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
