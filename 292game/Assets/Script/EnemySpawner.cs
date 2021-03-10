1e6f40e57ecfdb656caff771f207e88d5d0eae49
DK <Dikshyant99@gmail.com>
2021-03-09 20:27:42 -0600

    Background music and random spawner

---
+++ 292game/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int xpos;
+    public int ypos;
+    public int enemycount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop(){
+        while (enemycount < 2){
+            xpos = Random.Range (76,77);
+            ypos = Random.Range (46,-62);
+            Instantiate(enemy,new Vector3 (xpos,ypos,0), Quaternion.identity);
+            yield return new WaitForSeconds(10);
+            enemycount += 1;
+        }
+    }
+}
